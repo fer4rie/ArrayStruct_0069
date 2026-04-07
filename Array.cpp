416b631e892b7123761209bf1abc23a115e5542a
fer4rie <firapuspita2106@gmail.com>
2026-04-07 14:58:33 +0700

    isi array datamu satu per satu

--- Array.cpp
+++ Array.cpp
@@ -15,3 +15,15 @@ int main (){
     //cara menulis atau mengisi array
     dataku[2] = 600;
     cout << "Data ke - 3 yang baru : " << dataku[2] << endl;
+
+    //Mengisi array datamu satu per satu
+    cout << "Data index ke - 0 : " ;
+    cin >> datamu[0];
+    cout << "Data index ke - 1 : " ;
+    cin >> datamu[1];
+    cout << "Data index ke - 2 : " ;
+    cin >> datamu[2];
+    cout << "Data index ke - 3 : " ;
+    cin >> datamu[3];
+    cout << "Data index ke - 4 : " ;
+    cin >> datamu[4];
