b5bf09bdbfd8029cd67a6dab05ea9ae86e6964ba
fer4rie <firapuspita2106@gmail.com>
2026-04-07 15:00:21 +0700

    menampilkan array datamu satu per satu

--- Array.cpp
+++ Array.cpp
@@ -27,3 +27,12 @@ int main (){
     cin >> datamu[3];
     cout << "Data index ke - 4 : " ;
     cin >> datamu[4];
+
+    //Tampilkan array datamu satu per satu
+    cout << "Data ke - 1 : " << datamu[0] << endl;
+    cout << "Data ke - 2 : " << datamu[1] << endl;
+    cout << "Data ke - 3 : " << datamu[2] << endl;
+    cout << "Data ke - 4 : " << datamu[3] << endl;
+    cout << "Data ke - 5 : " << datamu[4] << endl;
+
+    cout << endl;
