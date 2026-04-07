18d87480dc85f1bbc1846dc63163f86c93eed9a4
fer4rie <firapuspita2106@gmail.com>
2026-04-07 15:13:34 +0700

    deklarasi dan tampilkan obyek dari struct orang

--- Struct.cpp
+++ Struct.cpp
@@ -6,3 +6,21 @@ struct orang{
     string alamat;
     int umur;
 };
+
+int main ()
+{
+    //Deklarasi obyek dari struct orang
+    orang mhs;
+    //isi objek
+    cout << "Nama Mahasiswa:";
+    cin >> mhs.nama;
+    cout << "Alamat asal:";
+    cin >> mhs.alamat;
+    cout << "Umur:";
+    cin >> mhs.umur;
+    cout << endl;
+    //Menampilkan Isi Obyek
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
