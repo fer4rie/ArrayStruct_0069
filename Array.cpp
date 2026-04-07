2f84bf77d9cca72d8a8d53c9832eecff00c0e58a
fer4rie <firapuspita2106@gmail.com>
2026-04-07 15:03:44 +0700

    menampilkan data dengan loop for

--- Array.cpp
+++ Array.cpp
@@ -45,3 +45,11 @@ int main (){
     }
 
     cout<<endl;
+
+    //Menampilkan data dengan perulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke - " << i+1 << " : "<< datamu[i] << endl;
+    }
+}
+
